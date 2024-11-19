8bfe4d3e204f9f3b4b75a4e751bb748b80bb5741
Jyoti <jyotis0401@gmail.com>
2024-11-19 17:24:14 +0530

    first commit

---
+++ src/main/java/com/example/yummyapp/YummyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.yummyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyAppApplication.class, args);
+	}
+
+}
